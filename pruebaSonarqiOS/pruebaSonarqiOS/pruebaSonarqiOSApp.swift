21a37b97c0d56ea6c72366f284dda54565fff9ef
jmendizabaln <112492392+jmendizabaln@users.noreply.github.com>
2022-11-15 12:29:43 -0600

    Primer Commit

---
+++ pruebaSonarqiOS/pruebaSonarqiOS/pruebaSonarqiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  pruebaSonarqiOSApp.swift
+//  pruebaSonarqiOS
+//
+//  Created by Juan Manuel Mendizabal on 15/11/22.
+//
+
+import SwiftUI
+
+@main
+struct pruebaSonarqiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
